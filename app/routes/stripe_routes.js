0801abb75dab98aa272ec1fa12287925dadc36c1
mkzarra <mkzarra@gmail.com>
2018-08-08 13:38:25 -0400

    began integrating stripe with ashley

--- app/routes/stripe_routes.js
+++ app/routes/stripe_routes.js
@@ -5,6 +5,7 @@ const passport = require('passport')
 
 // pull in Mongoose model for stripes
 const Stripe = require('../models/stripe')
+const Order = require('../models/order')
 
 // we'll use this to intercept any errors that get thrown and send them
 // back to the client with the appropriate status code
@@ -19,20 +20,31 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // CREATE
-// POST /stripes
-router.post('/stripes', requireToken, (req, res) => {
+// POST /charge
+router.post('order/:id/charge', requireToken, (req, res, next) => {
   // set owner of new stripe to be current user
-  req.body.stripe.owner = req.user.id
+  const orderId = req.params.id
+  const stripeToken = req.body.stripeToken
+  const charge = {
+    amount: total,
+    currency: 'USD',
+    card: stripeToken
+  }
 
-  Stripe.create(req.body.stripe)
-    // respond to succesful `create` with status 201 and JSON of new "stripe"
-    .then(stripe => {
-      res.status(201).json({ stripe: stripe.toObject() })
-    })
-    // if an error occurs, pass it off to our error handler
-    // the error handler needs the error message and the `res` object so that it
-    // can send an error message back to the client
-    .catch(err => handle(err, res))
+  Stripe.charges.create(charge)
+    .then(Order.update(orderId, req.body))
+    .then(data => console.log('success ', data))
+    .catch(console.error('failure ', error))
+
+  // Stripe.create(req.body.stripe)
+  //   // respond to succesful `create` with status 201 and JSON of new "stripe"
+  //   .then(stripe => {
+  //     res.status(201).json({ stripe: stripe.toObject() })
+  //   })
+  //   // if an error occurs, pass it off to our error handler
+  //   // the error handler needs the error message and the `res` object so that it
+  //   // can send an error message back to the client
+  //   .catch(err => handle(err, res))
 })
 
 module.exports = router
